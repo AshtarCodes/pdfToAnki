01bdb659eacc73cb49744261abacf418837bdd08
Ashtar Paniagua <74083145+AshtarCodes@users.noreply.github.com>
2024-11-19 20:44:19 -0500

    added --chapter to help

--- js/index.js
+++ js/index.js
@@ -138,8 +138,10 @@ function printHelp() {
   console.log("-, --in                     read file from stdin");
   console.log("--file={FILENAME}           read file from {FILENAME}");
   console.log("");
+  console.log("--chapter {CHAPTER}       The chapter to look for.");
+  console.log("");
   console.log(
-    "--deckName {DECKNAME}       The name of the anki deck to add the notes to"
+    "--deckName {DECKNAME}       The name of the anki deck to add the notes to."
   );
   console.log("");
 }
